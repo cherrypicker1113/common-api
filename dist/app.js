d50284776e8ec12df5d993be0351327b6122e5de
Kim Yongcheon <cherrypicker1113@gmail.com>
2021-10-03 23:17:23 +0900

    dist 폴더 삭제

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-app.get("/", function (req, res, next) {
-    res.send("hello typescript express!");
-});
-exports.default = app;
